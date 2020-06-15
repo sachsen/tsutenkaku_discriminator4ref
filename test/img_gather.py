da8a63b566d3e4886dd731386dc3230e5efff6e6
miamashi <rinmei.pinecone@gmail.com>
2020-06-15 13:09:02 +0900

    ファイル保存場所の選択を追加
    ファイル保存場所の追加と不要な部分の削除をしました

--- test/img_gather.py
+++ test/img_gather.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import random
 
 def image(word,i):
     response = requests.get("https://www.google.co.jp/search?q="+word+"&tbm=isch") #HTMLの取得
@@ -9,21 +8,17 @@ def image(word,i):
     link = links[i].get("src")
     return link
 
-def download_img(url, file_name):
+def download_img(url, file_name, path):
     r = requests.get(url, stream=True) #stream=Trueは処理を速める
     if r.status_code == 200:# webpageが機能してるか判断 例えば存在しない場合は404となる
-        with open(file_name+".jpeg", 'wb') as f:#open(ファイル名,何をするか) as 一時ファイル名（fが一般的）
+        with open(path+file_name + ".jpeg", 'wb') as f:#open(ファイル名,何をするか) as 一時ファイル名（fが一般的）
             f.write(r.content)
 
-def code():
-    code = ""
-    for i in range(10):
-        code += random.choice("0123456789")
-    return code
 
-num = input("検索回数:")
-word = input("検索ワード:")
-for i in range(1,int(num)+1): #0から始めるとグーグルのアイコンを読み込んでバグる（謎）
+num = input("検索回数:")         #ここに画像枚数
+word = input("検索ワード:")      #ここに画像の検索ワード
+path = "C:/"                    #ここに保存する場所を入力　注意点として\を/に置換すること　例(desktop):C:/Users/ユーザー名/Desktop/
+for i in range(1,int(num)+1):  #0から始めるとグーグルのアイコンを読み込んでバグる（謎）
     link = image(word,i)
-    download_img(link,str(i))
+    download_img(link,str(i),path)
 print("OK")
